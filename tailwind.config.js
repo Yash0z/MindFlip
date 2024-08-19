2f4bbd41753d14906131b6e628633934c03b9480
kai <yash.amberkar005@gmail.com>
2024-08-19 22:18:43 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      screens: {
+         'lg': "1694px",
+      },
+      extend: {
+         fontFamily: {
+            Euclid_re: ['Euclid_re'],
+            Euclid_li: ['Euclid_li'],
+            Raleway_re: ['raleway_re'],
+            PlaywriteEN: ['PlaywriteEN'],
+         },
+         colors: {
+            background: 'rgba(var(--background))',
+            active: 'rgba(var(--active))',
+            filled: 'rgba(var(--filled))',
+            accent1: 'rgba(var(--accent-1))',
+            accent2: 'rgba(var(--accent-2))',
+            accent3: 'rgba(var(--accent-3))',
+            textclr1: 'rgba(var(--text-clr1))',
+            textclr2: 'rgba(var(--text-clr2))',
+         },
+         typoraphy: {
+            DEFAULT: {
+               css: {
+                  "*": {
+                     margin: 0,
+                     padding: 0,
+                     listStyle: "none",
+                     boxSizing: "border-box",
+                     textDecoration: "none",
+                     fontFamily: "Questrial",
+                     color: "inherit",
+                  },
+               },
+            },
+         },
+      },
+   },
+   plugins: [],
+}
+
