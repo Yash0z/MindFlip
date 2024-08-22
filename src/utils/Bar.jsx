2e35215697d95d97d1ecbdaa40c3c1aa0e1cc8a3
kai <yash.amberkar005@gmail.com>
2024-08-22 10:05:38 +0530

    mapped props to classNames

--- src/utils/Bar.jsx
+++ src/utils/Bar.jsx
@@ -1,10 +1,40 @@
 const Bar = ({ clr, pos, w, h, axis }) => {
+	const axisVariants = {
+		x: "border-x",
+		y: "border-y",
+	};
+
+	const posVariants = {
+		top: "top-0",
+		bottom: "bottom-0",
+		left: "left-0",
+		right: "right-0",
+	};
+
+	const widthVariants = {
+		full: "w-full",
+		3: "w-3",
+	};
+
+	const heightVariants = {
+		full: "h-full",
+		3: "h-3",
+	};
+
+	const colorVariants = {
+		purple: "bg-purple",
+		yellow: "bg-yellow",
+		red: "bg-red",
+		green: "bg-green",
+		blue: "bg-blue",
+		orange: "bg-orange",
+	};
+
 	return (
-		<>
-			<div
-				className={`border-${axis} border-black absolute h-${h} w-${w} ${pos}-0 bg-${clr} `}
-			></div>
-		</>
+		<div
+			className={`absolute border-black ${axisVariants[axis]} ${posVariants[pos]} ${widthVariants[w]} ${heightVariants[h]} ${colorVariants[clr]}`}
+		></div>
 	);
 };
+
 export default Bar;
