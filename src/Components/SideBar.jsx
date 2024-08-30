012b66f87d987bf9f412be4b0c4687d15abcf4fc
kai <yash.amberkar005@gmail.com>
2024-08-30 22:14:55 +0530

    toggle state stuff

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,20 +1,28 @@
 import { IoReorderThreeOutline } from "react-icons/io5";
 import HamBurger from "../utils/HamBurger";
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+	toggleSidebar,
+	sidebarState,
+} from "../Store/SidebarSlice/sidebarSlice";
 
 const Sidebar = () => {
-	const [toggle, setToggle] = useState(false);
+	const dispatch = useDispatch();
+	const isOpen = useSelector(sidebarState);
+
 	const handleClick = () => {
-		setToggle(!toggle);
+		dispatch(toggleSidebar());
 		// console.log(toggle);
 	};
+
 	return (
 		<>
 			<HamBurger click={handleClick} />
 			<div className='fixed inset-0'>
 				<div
 					className={`${
-						toggle === true ? "block" : " hidden"
+						isOpen ? "block" : " hidden"
 					} absolute right-0 bg-white h-screen w-[40%]`}
 				>
 					das
--- src/Store/App/store.js
+++ src/Store/App/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cardReducer from "../CardSlice/cardSlice";
+import cardReducer from "../CardSlice/cardSlice.js";
+import sidebarReducer from "../SidebarSlice/sidebarSlice.js"
+
 
 export const store = configureStore({
    reducer: {
-      cards: cardReducer
+      cards: cardReducer,
+      sidebar: sidebarReducer,
    }
 })
